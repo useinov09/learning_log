39d0d7ebf63a3cedc06c2a427576dcf15d5f05d6
Edem Useinov <edemuseinov322@gmail.com>
2026-05-01 16:11:55 +0300

    Add Topic model

---
+++ learning_logs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Topic(models.Model):
+    """Тема, которую изучает пользователь."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Возвращает строковое представление модели."""
+        return self.text
