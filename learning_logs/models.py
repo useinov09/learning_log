b3ad1d8048a8be79c5ed8825b67261619796adb9
Edem Useinov <edemuseinov322@gmail.com>
2026-05-24 18:12:28 +0300

    Reduce Topic.text max_length from 200 to 100

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """Тема, которую изучает пользователь."""
-    text = models.CharField(max_length=200)
+    text = models.CharField(max_length=100)
     date_added = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
